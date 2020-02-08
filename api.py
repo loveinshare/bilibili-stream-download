21f2b7a2bf21606c53f56e70cf3327c215627de5
gen <sv@e3.com>
2020-02-08 21:57:21 +0800

    2.8

--- api.py
+++ api.py
@@ -1,5 +1,7 @@
 import urllib
 import json
+import re
+import time
 uid = "9999"
 live_api = "https://api.live.bilibili.com/room/v1/Room/room_init?id=%s"%uid
 stream_api = "https://api.live.bilibili.com/room/v1/Room/playUrl?cid=%s&quality=4&platform=web"%uid
@@ -29,5 +31,32 @@ def get_stream_url(uid):
     
     rtn = my_request(stream_api)
     urls = rtn.get("data").get("durl")
+    print(urls)
+    retry_time= 0
     if urls:
-        return urls[0].get("url")
+        while retry_time <20:
+            for i in urls:
+                for referer in [True,False]:
+                    try :
+                        retry_time+=1
+                        url = i.get("url")
+                        headers = dict()
+                        headers['Accept-Encoding'] = 'identity'
+                        # headers['User-Agent'] = 'Mozilla/5.0 (Windows NT 6.1; Trident/7.0; rv:11.0) like Gecko'
+                        headers["UserAgent"] = "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.71 Safari/537.36 " 
+                        if referer == True:
+                            headers['Referer'] = re.findall(r'(https://.*\/).*\.flv', url)[0]
+                        req = urllib.request.Request(url,headers=headers)
+                        res = urllib.request.urlopen(req)
+                        
+                        return i.get("url"),referer
+
+                    except Exception as e :
+                        time.sleep(1)
+                        print("retry",retry_time,"]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]")
+                        print(url)
+                        print(e)
+                        retry_time+=1
+                        pass
+            
+        
--- main.py
+++ main.py
@@ -5,53 +5,67 @@ import time,datetime
 from urllib import request
 import urllib
 import socket
-socket.setdefaulttimeout(5.0) 
+socket.setdefaulttimeout(10.0) 
 import threading
 import streamlink
-
+import re
 import sys
 
 import traceback
 from api import is_live,get_stream_url
 
-def record(url, file_name):
+def record(url, file_name,referer =True):
     if not url:
         return
 
     res = None
     output_file = None
-
-    try:
-        headers = dict()
-        headers['Accept-Encoding'] = 'identity'
-        headers['User-Agent'] = 'Mozilla/5.0 (Windows NT 6.1; Trident/7.0; rv:11.0) like Gecko'
-        #headers['Referer'] = re.findall(r'(https://.*\/).*\.flv', url)[0]
-        #res = requests.get(url, stream=True, headers=headers)
-        #res = request.urlopen(url, timeout=100000,headers=headers)
-        r = urllib.request.Request(url,headers=headers)
-        res = urllib.request.urlopen(r)
-        with open(file_name, 'wb') as f:
+    retry_num = 0
+    
+    headers = dict()
+    headers["UserAgent"] = "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.71 Safari/537.36 " 
+    #headers['Accept-Encoding'] = 'identity'
+    #headers['User-Agent'] = 'Mozilla/5.0 (Windows NT 6.1; Trident/7.0; rv:11.0) like Gecko'
+    print("referer",referer)
+    # if referer == True:
+    #     headers['Referer'] = re.findall(r'(https://.*\/).*\.flv', url)[0]
+    #res = requests.get(url, stream=True, headers=headers)
+    #res = request.urlopen(url, timeout=100000,headers=headers)
+    r = urllib.request.Request(url,headers=headers)
+    print(url)
+    while retry_num <10 :
+        try :
+            res = urllib.request.urlopen(r)
+            break
+        except Exception as e:
+            print(retry_num,"=============================")
+            print(e)
+            print("=============================")
+            retry_num +=1
+            time.sleep(2)
+    if not res:
+        return        
         
             
+    
+    try:
+        with open(file_name, 'wb') as f:    
             print('starting download from:\n%s\nto:\n%s' % (url, file_name))
-
             size = 0
-            _buffer = res.read(1024 * 256)
-            n = 0
-            while n <10 :
-                if len( _buffer) == 0:
-                    n+=1
-                else:
-                    n = 0
+            _buffer = res.read(1024 *256)
+            
+            while 1 :
+     
                     #print(len(_buffer))
                     f.write(_buffer)
                     size += len(_buffer)
                     print('{:<4.2f} MB downloaded'.format(size/1024/1024),datetime.datetime.now(),end="\r")
-                    sys.stdout.flush()
-                    _buffer = res.read(1024 * 256)
+                    #sys.stdout.flush()
+                    _buffer = res.read(1024 * 32)
     except Exception as e:
+        print("=============================")
         print(e)
-    
+        print("=============================")
     finally:
         print("finnally")
         if res:
@@ -101,8 +115,7 @@ while 1 :
         time.sleep(5)
         pass
     else:
-        if email_status == 1:
-            email_Sender.send("开播了")              
+                   
         now = datetime.datetime.now()
         # real_url = None
         # for k,v in streams.items():
@@ -122,7 +135,7 @@ while 1 :
         #     except Exception as e:
         #         print(e)
         #         pass
-        real_url = get_stream_url(_id)
+        real_url,referer = get_stream_url(_id)
         if real_url == None:
             print("开播了但是没有源")
             now  = datetime.datetime.now()
@@ -134,7 +147,9 @@ while 1 :
         filename =_path+ _name +now.strftime("_%Y_%m_%d_%H_%M_%S_%f_"+"_.flv")
 
         #print("开始下载\n"+real_url)
-        record(real_url,filename)
+        if email_status == 1:
+            email_Sender.send("开播了")   
+        record(real_url,filename,referer)
             
 
         print("$$$$$$$$$$$allend")
